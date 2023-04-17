5cb1a0b65b0798e95494063ba772b4b238238919
Vágó Rebeka <79878482+vagorebeka@users.noreply.github.com>
2023-04-17 04:17:49 +0200

    Tried login, throws database error (table not found)

--- new/gear-helper_Admin-master/GearHelper_Admin/Login.xaml.cs
+++ new/gear-helper_Admin-master/GearHelper_Admin/Login.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,17 +13,13 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Data;
-using System.Data.SqlClient;
-using System.Text.RegularExpressions;
 
 namespace GearHelper_Admin
 {
-    /// <summary>  
-    /// Interaction logic for MainWindow.xaml  
-    /// </summary>   
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
     public partial class Login : Window
     {
         public Login()
@@ -27,29 +27,42 @@ namespace GearHelper_Admin
             InitializeComponent();
         }
 
+        private void ValidateUser()
+        {
+            string query = "SELECT * from tbl_login WHERE name = @name and password=@password";
+            User user;
+            /*SqlConnectionStringBuilder connStr = new SqlConnectionStringBuilder()
+            {
+                Data Source = "localhost";
+            InitialCatalog = "gear-helper";
+            UserID = "root";
+            }.ConnectionString;*/
+            using (MySqlConnection con = new MySqlConnection("Server=localhost;Port=3306;User ID=username;Password=password;Database=gear-helper;")) //created a user with this data in phpmyadmin
+            {
+                using (MySqlCommand sqlcmd = new MySqlCommand(query, con))
+                {
+                    sqlcmd.Parameters.Add("@name", MySqlDbType.VarChar).Value = usernameBox.Text;
+                    sqlcmd.Parameters.Add("@password", MySqlDbType.VarChar).Value = passwordBox.Text;
+                    con.Open();
+                    user = (User)sqlcmd.ExecuteScalar();
+                }
+            }
+            if (user.Admin)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Hide();
+            }
+            else
+            {
+                infoLabel.Content = "Incorrect username or password";
+                return;
+            }
+        }
+
         private void loginButton_Click(object sender, RoutedEventArgs e)
         {
-            string user = usernameBox.Text;
-            string pass = passwordBox.Text;
-            using (SqlConnection conn = new SqlConnection("server=localhost; database=gear-helper; user id=root"))
-            {
-                conn.Open();
-                SqlCommand command = new SqlCommand("select * from mysqlcsharp where username = @Username and password = @Password", conn);
-                command.Parameters.AddWithValue("@Username", user);
-                command.Parameters.AddWithValue("@Password", pass);
-                SqlDataReader reader = command.ExecuteReader();
-                if (reader.Read() == true)
-                {
-                    MessageBox.Show("OK");
-                    MainWindow mainWindow = new MainWindow();
-                    mainWindow.Show();
-                }
-                else
-                {
-                    MessageBox.Show("Incorrect Username or Password");
-                }
-                conn.Close();
-            }
+            ValidateUser();
         }
     }
 }
